29a5a769df915e8eab1ab91903d60b14d045056c
aliarnoos <alimarnoos@gmail.com>
2023-06-26 16:52:03 +0300

    add login component

---
+++ src/components/Login.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="flex justify-center items-center min-h-screen bg-gray-100">
+    <div class="bg-white p-8 shadow-md rounded-md w-80">
+      <h2 class="text-2xl font-bold mb-4">Login</h2>
+      <form @submit.prevent="login">
+        <div class="mb-4">
+          <label for="email" class="block text-gray-700 text-sm font-bold mb-2"
+            >Email</label
+          >
+          <input
+            v-model="email"
+            type="email"
+            id="email"
+            class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-green-500"
+            required
+          />
+        </div>
+        <div class="mb-6">
+          <label
+            for="password"
+            class="block text-gray-700 text-sm font-bold mb-2"
+            >Password</label
+          >
+          <input
+            v-model="password"
+            type="password"
+            id="password"
+            class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-green-500"
+            required
+          />
+        </div>
+        <button
+          type="button"
+          @click="login"
+          class="w-full py-2 px-4 bg-green-500 text-white font-bold rounded hover:bg-green-700"
+        >
+          Login
+        </button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const email = ref("");
+    const password = ref("");
+
+    const login = () => {
+      console.log(email.value, password.value);
+    };
+
+    return {
+      email,
+      password,
+      login,
+    };
+  },
+};
+</script>
