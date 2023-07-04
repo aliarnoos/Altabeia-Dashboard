3de45d907591caf0f2a5d26bb603b03ea48a8147
aliarnoos <alimarnoos@gmail.com>
2023-07-04 15:45:23 +0300

    add request store to handle request for all modules

---
+++ src/stores/request.ts
@@ -0,0 +1,47 @@
+import { ref } from "vue";
+import { defineStore } from "pinia";
+import {
+  deleteRequest,
+  getRequest,
+  patchRequest,
+  postRequest,
+} from "../api/httpRequest";
+
+export const useRequestStore = defineStore("request", () => {
+  const fetchedData = ref();
+
+  const getData = async (url: string, token?: string) => {
+    try {
+      const response = await getRequest(url, token);
+      fetchedData.value = response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const updateData = async (url: string, data: {}, token: string) => {
+    try {
+      return await patchRequest(url, data, token);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const postData = async (url: string, data: {}, token: string) => {
+    try {
+      return await postRequest(url, data, token);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const deleteData = async (url: string, token: string) => {
+    try {
+      return await deleteRequest(url, token);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return { fetchedData, getData, updateData, postData, deleteData };
+});
