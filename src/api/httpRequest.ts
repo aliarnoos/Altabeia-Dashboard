7ff4e221405fc084ca99e6076c8155db24d55346
aliarnoos <alimarnoos@gmail.com>
2023-07-04 15:44:38 +0300

    Update xhr functions

--- src/api/httpRequest.ts
+++ src/api/httpRequest.ts
@@ -1,12 +1,38 @@
-const sendHttpRequest = (
-  method: string,
-  url: string,
-  data?: {},
-  token?: string
-) => {
+const getRequest = (url: string, token?: string) => {
   const promise: Promise<any> = new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
-    xhr.open(method, url);
+    xhr.open("GET", url);
+
+    xhr.responseType = "json";
+
+    if (token) {
+      xhr.setRequestHeader("Authorization", `Bearer ${token}`);
+    }
+    xhr.withCredentials = true;
+
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        resolve(xhr.response);
+      } else if (xhr.status === 404) {
+        reject("Not found");
+      } else {
+        reject(xhr.response);
+      }
+    };
+
+    xhr.onerror = () => {
+      reject("Something went wrong!");
+    };
+
+    xhr.send();
+  });
+  return promise;
+};
+
+const postRequest = (url: string, data: {}, token?: string) => {
+  const promise: Promise<any> = new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", url);
 
     xhr.responseType = "json";
 
@@ -17,12 +43,13 @@ const sendHttpRequest = (
       xhr.setRequestHeader("Content-Type", "application/json");
     }
     xhr.withCredentials = true;
-
     xhr.onload = () => {
-      if (xhr.status >= 400) {
-        reject(xhr.response);
-      } else {
+      if (xhr.status === 201) {
         resolve(xhr.response);
+      } else if (xhr.status === 404) {
+        reject("Not found");
+      } else {
+        reject(xhr.response);
       }
     };
 
@@ -35,4 +62,67 @@ const sendHttpRequest = (
   return promise;
 };
 
-export default sendHttpRequest;
+const patchRequest = (url: string, data: {}, token: string) => {
+  const promise: Promise<any> = new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("PATCH", url);
+
+    xhr.responseType = "json";
+
+    if (token) {
+      xhr.setRequestHeader("Authorization", `Bearer ${token}`);
+    }
+    if (data) {
+      xhr.setRequestHeader("Content-Type", "application/json");
+    }
+    xhr.withCredentials = true;
+
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        resolve(xhr.response);
+      } else if (xhr.status === 304) {
+        reject("Not Modified");
+      } else {
+        reject(xhr.response);
+      }
+    };
+
+    xhr.onerror = () => {
+      reject("Something went wrong!");
+    };
+
+    xhr.send(JSON.stringify(data));
+  });
+  return promise;
+};
+
+const deleteRequest = (url: string, token: string) => {
+  const promise: Promise<any> = new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("DELETE", url);
+
+    xhr.responseType = "json";
+
+    if (token) {
+      xhr.setRequestHeader("Authorization", `Bearer ${token}`);
+    }
+    xhr.withCredentials = true;
+
+    xhr.onload = () => {
+      if (xhr.status === 201) {
+        resolve(xhr.response);
+      } else if (xhr.status === 404) {
+        reject("Not found");
+      } else {
+        reject(xhr.response);
+      }
+    };
+
+    xhr.onerror = () => {
+      reject("Something went wrong!");
+    };
+  });
+  return promise;
+};
+
+export { getRequest, postRequest, patchRequest, deleteRequest };
