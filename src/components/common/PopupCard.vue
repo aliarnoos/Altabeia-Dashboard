4fd4bdb584f41ec51f373c478154d00ca4ce4674
aliarnoos <alimarnoos@gmail.com>
2023-07-11 14:53:17 +0300

    change PopupCard position

--- src/components/common/PopupCard.vue
+++ src/components/common/PopupCard.vue
@@ -2,13 +2,13 @@
   <div class="z-50">
     <p
       v-if="statusMessageStore.message == 'Something went wrong!'"
-      class="bg-red-500 text-white px-4 py-2 rounded absolute top-5 right-10 m-4 drop-shadow-md z-20"
+      class="bg-red-500 text-white px-4 py-2 rounded absolute top-10 left-1/2 transform -translate-x-1/2 -translate-y-1/2 m-4 drop-shadow-md z-20"
     >
       {{ statusMessageStore.message }}
     </p>
     <p
       v-else-if="statusMessageStore.message"
-      class="bg-green-500 text-white px-4 py-2 rounded absolute top-5 right-10 m-4 drop-shadow-md z-20"
+      class="bg-green-500 text-white px-4 py-2 rounded absolute top-10 left-1/2 transform -translate-x-1/2 -translate-y-1/2 m-4 drop-shadow-md z-20"
     >
       {{ statusMessageStore.message }}
     </p>
