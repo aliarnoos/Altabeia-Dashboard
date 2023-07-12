5f4ad3cd75fa5287f8d40fb03e6108f4988e31e8
aliarnoos <alimarnoos@gmail.com>
2023-07-12 16:26:46 +0300

    add logout fun

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -6,7 +6,7 @@
       <h1 class="text-2xl font-bold">Altabeia</h1>
     </div>
     <div class="flex items-center">
-      <span class="mr-4">{{ userStore.user?.name }}</span>
+      <span class="mr-4">{{ userStore.user?.email }}</span>
       <button
         @click="logout"
         class="px-4 py-2 bg-gray-700 hover:bg-gray-800 text-white rounded"
@@ -18,12 +18,30 @@
 </template>
 
 <script setup lang="ts">
+import { useLoadingStore } from "@/stores/loading";
+import { useRequestStore } from "@/stores/request";
+import { useTokenStore } from "@/stores/token";
 import { useUserStore } from "@/stores/user";
+import { useRouter } from "vue-router";
 
 const userStore = useUserStore();
-
-const logout = () => {
-  // Implement your logout logic here
+const tokenStore = useTokenStore();
+const requestStore = useRequestStore();
+const router = useRouter();
+const useLoading = useLoadingStore();
+const logout = async () => {
+  useLoading.setLoading();
+  const response = await requestStore.postData(
+    `${import.meta.env.VITE_API_URL}/admin/logout`,
+    {},
+    tokenStore.token || ""
+  );
+  if (response) {
+    router.push("/");
+    tokenStore.setNull();
+    useLoading.setFalse();
+    router.push("/login");
+  }
 };
 </script>
 
