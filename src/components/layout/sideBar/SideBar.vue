473c7c954a235e5d711b65390fd39abca1ca6e6f
aliarnoos <alimarnoos@gmail.com>
2023-07-12 16:27:02 +0300

    change layout

--- src/components/layout/sideBar/SideBar.vue
+++ src/components/layout/sideBar/SideBar.vue
@@ -2,87 +2,37 @@
   <ChangePassword
     v-if="ChangePasswordState"
     @cancelEdit="ChangePasswordState = false"
-    @statusMessage="(event) => showStatusMessage(event)"
   />
   <div class="flex flex-col bg-gray-800 text-white min-h-screen w-64 basis-1/6">
-    <div class="flex flex-col items-center justify-center py-6">
-      <h1 class="text-2xl font-bold">Dashboard</h1>
+    <div class="flex flex-col py-6">
+      <h1 class="text-lg text-center">My Profile</h1>
       <button
         @click="toogleUserDropDown"
-        class="m-2 p-2 flex justify-center items-center gap-1 hover:text-green-400 font-bold"
+        class="m-2 p-2 flex gap-1 hover:text-green-400 font-bold"
       >
-        <span v-if="showUserDropDown" class="material-symbols-outlined">
-          arrow_drop_up
-        </span>
-        <span v-else class="material-symbols-outlined"> arrow_drop_down </span>
-        {{ userStore.user?.email }}
+        <span class="material-symbols-outlined"> home </span>
+        Home
       </button>
-      <div
-        v-if="showUserDropDown"
-        class="flex flex-col m-2 p-4 w-full bg-gray-700"
-      >
-        <button
-          @click="logout"
-          class="m-2 flex justify-center items-center gap-1 hover:bg-red-500 rounded p-2"
-        >
-          <span class="material-symbols-outlined text-lg"> logout </span>
-          Logout
-        </button>
-        <button
-          class="m-2 flex justify-center items-center gap-1 hover:bg-green-500 rounded p-2"
-          @click="ChangePasswordState = true"
-        >
-          <span class="material-symbols-outlined text-lg"> edit </span>
 
-          Change Password
-        </button>
+      <div v-if="showUserDropDown" class="w-full bg-gray-900">
+        <SideBarButton :links="links" />
       </div>
+      <button class="m-2 p-2 flex gap-1 hover:text-green-400 font-bold">
+        <span class="material-symbols-outlined"> call </span>
+        Contact
+      </button>
     </div>
-    <SideBarButton :links="links" />
   </div>
-  <p
-    v-if="statusMessage == 'Something went wrong!'"
-    class="bg-red-700 text-white px-4 py-2 rounded absolute top-10 right-10 m-4 drop-shadow-md z-20"
-  >
-    {{ statusMessage }}
-  </p>
-  <p
-    v-else-if="statusMessage"
-    class="bg-green-700 text-white px-4 py-2 rounded absolute top-10 right-10 m-4 drop-shadow-md z-20"
-  >
-    {{ statusMessage }}
-  </p>
 </template>
 
 <script setup lang="ts">
-import { useRequestStore } from "@/stores/request";
-import { useTokenStore } from "@/stores/token";
-import { useUserStore } from "@/stores/user";
 import { ref } from "vue";
-import { useRouter } from "vue-router";
 import ChangePassword from "../../auth/ChangePassword.vue";
 import SideBarButton from "./sideBarButton.vue";
-
-const tokenStore = useTokenStore();
-const router = useRouter();
-const userStore = useUserStore();
-const requestStore = useRequestStore();
 
 const showUserDropDown = ref(false);
 
 const ChangePasswordState = ref(false);
-const statusMessage = ref();
-const showStatusMessage = (event: any) => {
-  if (event.value) {
-    statusMessage.value = event.value;
-  } else {
-    statusMessage.value = "Something went wrong!";
-  }
-  setTimeout(() => {
-    statusMessage.value = null;
-  }, 2000);
-};
-
 const toogleUserDropDown = () => {
   showUserDropDown.value = !showUserDropDown.value;
 };
@@ -96,17 +46,4 @@ const links = ref([
   { id: 5, label: "Teachers", path: "/teachers" },
   { id: 6, label: "Schools", path: "/schools" },
 ]);
-
-const logout = async () => {
-  const response = await requestStore.postData(
-    `${import.meta.env.VITE_API_URL}/admin/logout`,
-    {},
-    tokenStore.token || ""
-  );
-  if (response) {
-    router.push("/");
-    tokenStore.setNull();
-    router.push("/login");
-  }
-};
 </script>
