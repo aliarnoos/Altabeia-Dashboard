aa6b3a68fc48bd99a1a7bcb405f10fa885c9e770
aliarnoos <alimarnoos@gmail.com>
2023-07-16 16:45:59 +0300

    update icon

--- src/components/common/FilePreviewInput.vue
+++ src/components/common/FilePreviewInput.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="flex flex-col items-center gap-2">
+  <div class="flex flex-col gap-2">
     <label
       for="fileInput"
-      class="mt-2 px-4 py-2 border border-gray-300 rounded flex items-center justify-center cursor-pointer hover:bg-gray-300"
+      class="mt-2 px-4 py-2 border border-gray-300 rounded flex items-center justify-center gap-2 cursor-pointer hover:bg-gray-300"
     >
-      <span class="material-symbols-outlined"> attach_file </span>
+      <i class="fa-solid fa-paperclip"></i>
       <span>Choose File</span>
       <input
         id="fileInput"
