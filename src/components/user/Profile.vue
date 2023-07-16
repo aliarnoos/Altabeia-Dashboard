2fbde77a6f32ed38e25e90467e986ec99fdde649
aliarnoos <alimarnoos@gmail.com>
2023-07-16 11:56:47 +0300

    add user information display

--- src/components/user/Profile.vue
+++ src/components/user/Profile.vue
@@ -1,10 +1,25 @@
 <template>
-  <h1>profile</h1>
-  <div>
+  <div class="p-10 flex flex-col items-start gap-10 overflow-x-auto">
+    <h1 class="text-3xl font-bold underline underline-offset-8 unde">
+      Manage Profile
+    </h1>
+    <div class="border-b-4 pb-8 border-green-500">
+      <div class="flex gap-2 text-lg">
+        <p class="font-bold">Email:</p>
+        <p>{{ userStroe.user.email }}</p>
+      </div>
+      <div class="flex gap-2 text-lg">
+        <p class="font-bold">Role:</p>
+        <p>{{ userStroe.user.roles }}</p>
+      </div>
+    </div>
     <ChangePassword />
   </div>
 </template>
 
 <script setup lang="ts">
+import { useUserStore } from "@/stores/user";
 import ChangePassword from "../auth/ChangePassword.vue";
+
+const userStroe = useUserStore();
 </script>
