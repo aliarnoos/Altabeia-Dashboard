68b118971e5f08414b86392caf2b46c026507990
aliarnoos <alimarnoos@gmail.com>
2023-07-16 16:45:33 +0300

    fix reload error

--- src/components/user/Profile.vue
+++ src/components/user/Profile.vue
@@ -1,25 +1,28 @@
 <template>
   <div class="p-10 flex flex-col items-start gap-10 overflow-x-auto">
-    <h1 class="text-3xl font-bold underline underline-offset-8 unde">
+    <h1 class="text-3xl font-bold pb-2 border-b-4 border-green-500">
       Manage Profile
     </h1>
-    <div class="border-b-4 pb-8 border-green-500">
+    <!-- <div class="border-b-4 pb-8 border-green-500">
       <div class="flex gap-2 text-lg">
         <p class="font-bold">Email:</p>
-        <p>{{ userStroe.user.email }}</p>
+        <p>{{ email }}</p>
       </div>
       <div class="flex gap-2 text-lg">
         <p class="font-bold">Role:</p>
-        <p>{{ userStroe.user.roles }}</p>
+        <p>{{ role }}</p>
       </div>
-    </div>
+    </div> -->
     <ChangePassword />
   </div>
 </template>
 
 <script setup lang="ts">
-import { useUserStore } from "@/stores/user";
+// import { useUserStore } from "@/stores/user";
 import ChangePassword from "../auth/ChangePassword.vue";
 
-const userStroe = useUserStore();
+// const userStroe = useUserStore();
+
+// const email = userStroe.user.email;
+// const role = userStroe.user.roles;
 </script>
