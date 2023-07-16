2968bbec5c71fa949251dbece06b411c1ea2ab06
aliarnoos <alimarnoos@gmail.com>
2023-07-16 11:56:17 +0300

    add error message handler

--- src/components/auth/ChangePassword.vue
+++ src/components/auth/ChangePassword.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex justify-center items-center">
     <div class="bg-white">
-      <h1 class="text-lg font-bold mb-8 text-center">Change Password</h1>
+      <h1 class="text-lg font-bold mb-8">Change Password</h1>
       <form @submit.prevent="updatePassword" class="grid grid-cols-2 gap-4">
         <label for="password">Password:</label>
         <input
@@ -12,21 +12,21 @@
           required
           class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-green-600"
         />
-        <label for="password">New Password:</label>
+        <label for="newPassword">New Password:</label>
         <input
-          v-model="password"
+          v-model="newPassword"
           type="text"
-          name="password"
-          id="password"
+          name="newPassword"
+          id="newPassword"
           required
           class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-green-600"
         />
-        <label for="password">Confirm New Password:</label>
+        <label for="confPassword">Confirm New Password:</label>
         <input
-          v-model="password"
+          v-model="confPassword"
           type="text"
-          name="password"
-          id="password"
+          name="confPassword"
+          id="confPassword"
           required
           class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-green-600"
         />
@@ -36,14 +36,10 @@
           >
             Update
           </button>
-          <button
-            type="button"
-            @click="$emit('cancelEdit')"
-            class="w-full py-2 px-4 bg-gray-500 text-white font-bold rounded hover:bg-gray-600"
-          >
-            Cancel
-          </button>
         </div>
+        <p v-if="errorMessage" class="text-red-600 text-xs mb-3">
+          {{ errorMessage }}
+        </p>
       </form>
     </div>
   </div>
@@ -51,27 +47,35 @@
 
 <script setup lang="ts">
 import { useRequestStore } from "@/stores/request";
+import { useMessageStore } from "@/stores/statusMessage";
 import { useTokenStore } from "@/stores/token";
 import { ref } from "vue";
 
 const requestStore = useRequestStore();
 const tokenStore = useTokenStore();
-
-const emit = defineEmits(["cancelEdit", "statusMessage"]);
+const statusMessage = useMessageStore();
 
 const password = ref();
-const statusMessage = ref();
+const newPassword = ref();
+const confPassword = ref();
+const errorMessage = ref();
 
 const updatePassword = async () => {
-  const response = await requestStore.updateData(
-    `${import.meta.env.VITE_API_URL}/admin/me/password`,
-    { newPassword: password.value },
-    tokenStore.token || ""
-  );
-  if (response) {
-    statusMessage.value = response.message;
-    emit("cancelEdit");
+  if (newPassword.value != confPassword.value) {
+    errorMessage.value = "Passwords do not match!";
+  } else {
+    errorMessage.value = null;
+    const response = await requestStore.updateData(
+      `${import.meta.env.VITE_API_URL}/admin/me/password`,
+      { password: password.value, newPassword: newPassword.value },
+      tokenStore.token || ""
+    );
+    if (response) {
+      statusMessage.setMessage(response.message);
+    }
+    password.value = null;
+    newPassword.value = null;
+    confPassword.value = null;
   }
-  emit("statusMessage", statusMessage);
 };
 </script>
