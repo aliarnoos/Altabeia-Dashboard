4444433756a68ac03b97bbd766b640ee94e4c954
aliarnoos <alimarnoos@gmail.com>
2023-07-16 10:55:18 +0300

    add Activities and AddActivity components

---
+++ src/components/activities/Activities.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="p-10 flex items-center flex-col gap-10 overflow-x-auto">
+    <div class="flex items-center justify-between w-full">
+      <h1 class="text-3xl font-bold text-center">Activities</h1>
+      <RouterLink
+        to="/activities/add"
+        class="flex justify-center items-center gap-2 bg-green-500 rounded p-4 text-white ml-auto font-bold hover:bg-green-600"
+      >
+        <i class="fa-solid fa-plus"></i>
+        <p>Add Activity</p>
+      </RouterLink>
+    </div>
+    <table class="w-full">
+      <thead>
+        <tr>
+          <th class="p-2 border">ID</th>
+          <th class="p-2 border">Title</th>
+          <th class="p-2 border">Description</th>
+          <th class="p-2 border">Summary</th>
+          <th class="p-2 border">Date</th>
+          <th class="p-2 border">Visible</th>
+          <th class="p-2 border">Edit</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in items" :key="item.id">
+          <td class="border p-2 text-center">{{ item.id }}</td>
+          <td class="border p-2">{{ item.titleEn }}</td>
+          <td class="border p-2">{{ item.descriptionEn }}</td>
+          <td class="border p-2">{{ item.summaryEn }}</td>
+          <td class="border p-2">{{ item.date }}</td>
+          <td class="border p-4">
+            {{ item.isVisible ? "Yes" : "No" }}
+          </td>
+          <td class="border p-4 text-center">
+            <RouterLink
+              :to="`/activities/${item.id}`"
+              class="hover:text-green-600 font-bold p-2 pl-4 pr-4 rounded"
+            >
+              <i class="fa-solid fa-pen-to-square"></i>
+            </RouterLink>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useTokenStore } from "../../stores/token";
+import { useRequestStore } from "../../stores/request";
+import { onBeforeMount, ref } from "vue";
+import { useLoadingStore } from "@/stores/loading";
+
+interface Item {
+  id: number;
+  titleKu: string;
+  titleEn: string;
+  titleAr: string;
+  titleTu: string;
+  summaryKu: string;
+  summaryEn: string;
+  summaryAr: string;
+  summaryTu: string;
+  descriptionKu: string;
+  descriptionEn: string;
+  descriptionAr: string;
+  descriptionTu: string;
+  image: string;
+  imageUrl: string;
+  date: string;
+  isVisible: string;
+}
+const tokenStore = useTokenStore();
+const requestStore = useRequestStore();
+const loadingStore = useLoadingStore();
+
+const items = ref<Item[]>();
+
+const fetchActivities = async () => {
+  loadingStore.setLoading();
+  await requestStore.getData(
+    import.meta.env.VITE_API_URL + "/admin/activities",
+    tokenStore.token || undefined
+  );
+  items.value = requestStore.fetchedData.activities;
+  loadingStore.setFalse();
+};
+
+onBeforeMount(async () => {
+  fetchActivities();
+});
+</script>
