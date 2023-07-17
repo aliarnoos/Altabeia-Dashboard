cf507482b54784995bc9fe5c4efae26ba0c851dd
aliarnoos <alimarnoos@gmail.com>
2023-07-17 14:27:37 +0300

    fix position bug

--- src/components/common/PopupCard.vue
+++ src/components/common/PopupCard.vue
@@ -2,13 +2,14 @@
   <div class="z-50">
     <p
       v-if="statusMessageStore.message == 'Something went wrong!'"
-      class="bg-red-500 text-white px-4 py-2 rounded absolute top-10 left-1/2 transform -translate-x-1/2 -translate-y-1/2 m-4 drop-shadow-md z-20"
+      class="bg-red-500 text-white px-4 py-2 rounded fixed top-4 left-1/2 transform -translate-x-1/2 m-4 drop-shadow-md z-20"
     >
       {{ statusMessageStore.message }}
     </p>
+
     <p
       v-else-if="statusMessageStore.message"
-      class="bg-green-500 text-white px-4 py-2 rounded absolute top-10 left-1/2 transform -translate-x-1/2 -translate-y-1/2 m-4 drop-shadow-md z-20"
+      class="bg-green-500 text-white px-4 py-2 rounded fixed top-4 left-1/2 transform -translate-x-1/2 m-4 drop-shadow-md z-20"
     >
       {{ statusMessageStore.message }}
     </p>
