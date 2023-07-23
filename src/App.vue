b33ab655abe7654a97ca3c7b040bc4fffbc0dcd2
aliarnoos <alimarnoos@gmail.com>
2023-07-23 14:29:08 +0300

    delete old code

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,7 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
-</script>
-
 <template>
-  <!-- <header>
-    <div>
-      <nav>
-        <RouterLink to="/home">Home</RouterLink>
-        <RouterLink to="/">login</RouterLink>
-      </nav>
-    </div>
-  </header> -->
 
   <RouterView />
 </template>
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+</script>
