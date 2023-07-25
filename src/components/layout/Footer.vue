2a773ade688fd4b67bc55567ecbceee3b9136c31
aliarnoos <alimarnoos@gmail.com>
2023-07-25 10:20:30 +0300

    update footer

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,13 +1,16 @@
 <template>
   <footer class="bg-gray-700 text-white py-8 px-1 border-t-4 border-gray-800">
-    <div class="container mx-auto text-center">
-      <p>&copy; 2023 Admin Dashboard. All rights reserved.</p>
+    <div class="container mx-auto flex flex-col gap-4 text-center">
+      <p>&copy; {{ year }} Admin Dashboard. All rights reserved.</p>
+      <p>Developed by Black Ace</p>
     </div>
   </footer>
 </template>
 
-<script lang="ts">
-export default {
-  name: "DashboardFooter",
-};
+<script setup lang="ts">
+import { ref } from 'vue';
+
+
+
+const year = ref(new Date().getFullYear())
 </script>
